d0360c249ca18098db5a7814fd08529d13ea5a77
Keno Clayton <keno.clayton15@gmail.com>
2020-11-25 09:50:48 -0500

    first version (not working yet)

--- is_installed.sh
+++ is_installed.sh
@@ -10,14 +10,14 @@ COL_BLUE=$ESC_SEQ"34;01m"
 COL_MAGENTA=$ESC_SEQ"35;01m"
 COL_CYAN=$ESC_SEQ"36;01m"
 
-# Set true by default
+# Global Vars
 DEPS_INSTALLED="true"
+file1="programs.txt"
+file2="npm_packages.txt"
 
 # Functions ==============================================
 
-# return 1 if global command line program installed, else 0
-# example
-# echo "node: $(program_is_installed node)"
+# Returns 
 function program_is_installed() {
     # set to 1 initially
     local return_=1
@@ -27,7 +27,7 @@ function program_is_installed() {
     type $1 >/dev/null 2>&1 || { local return_=0; }
 
     # Check the program version
-    if [[ $return_ = 1 ]]; then
+    if [[ $return_ == 1 ]]; then
         local version=$(check_program_ver $1)
     fi
 
@@ -59,14 +59,14 @@ function npm_package_is_installed() {
 
 # Checks versions of programs
 function check_program_ver() {
+    echo $1
     if [[ $1 = "node" ]]; then
-        echo "$(node -v)"
-    elif [[ $1 = "python" ]]; then
-        echo "2"
-    elif [[ $1 = "python3" ]]; then
-        echo "3"
+        echo $(node -v)
     elif [[ $1 = "java" ]]; then
         echo $(java -version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1)
+    else
+        # Discards any errors into /dev/null and logs the version otherwise returns 'Unknown'
+        echo $(dpkg -s $1 2> /dev/null | grep Version | awk '{ print $2; }' || echo "Unknown")
     fi
 }
 
@@ -74,7 +74,7 @@ function check_program_ver() {
 function check_package_ver() {
     local ver=$(npm list $1 --depth=0 | awk -F@ '{print $2}')
     if [[ ! $ver ]]; then
-        ver=$(npm list -g $1 --depth=0 | awk -F@ '{print $2}')
+        local ver=$(npm list -g $1 --depth=0 | awk -F@ '{print $2}')
     fi
     echo $ver
 }
@@ -89,24 +89,56 @@ function echo_pass() {
     printf "$COL_GREEN ${1}%-3s \t✔ version: ${2} $COL_RESET\n"
 }
 
-# ============================================== Functions
+# Functions ============================================== 
 
-echo -e "$COL_CYAN Checking dependencies $COL_RESET"
+echo -e "$COL_CYAN Checking dependencies... $COL_RESET"
+echo -e "$COL_YELLOW Reading files... $COL_RESET"
+
+# TODO: Allow user to provide path for files
+
+missing_files=();
+# Check to ensure required files exist
+if [[ ! -f "$file1" ]]; then
+    missing_files+=("$file1")
+fi
+
+if [[ ! -f "$file2" ]]; then
+    missing_files+=("$file2")
+fi
+
+if [[ ${#missing_files[@]} -gt 0 ]]; then
+    printf "$COL_YELLOW Missing files: $COL_RESET" 
+    printf "$COL_RED"
+    for value in "${missing_files[@]}"
+    do
+        printf "$value "
+    done
+    printf "$COL_RESET"
+    echo -e "\n$COL_YELLOW Closing...$COL_RESET" 
+    exit 1
+fi
+
+# TODO: Check if files are empty with [ -s filename ]
 
 # Checks the program versions
-for dep in node python3 java; do
-    program_installed=($(program_is_installed $dep))
+while IFS= read -r line; do
+    sys_dep="$line"
+    echo $sys_dep
+    echo $(check_program_ver $sys_dep)
+    program_installed=($(program_is_installed $sys_dep))
+    echo $program_installed
     if [[ ! ${program_installed[2]} ]]; then
         DEPS_INSTALLED="false"
         echo_fail ${program_installed[1]}
     else
         echo_pass ${program_installed[1]} ${program_installed[2]}
     fi
-done
+done < "$file1"
 
 # Checks the npm package versions
-for dep in expo-cli turtle-cli; do
-    package_installed=($(npm_package_is_installed $dep))
+while IFS= read -r line; do
+    npm_dep="$line"
+    package_installed=($(npm_package_is_installed $npm_dep))
     if [[ ! ${package_installed[2]} ]]; then
         DEPS_INSTALLED="false"
         echo_fail ${package_installed[1]}
@@ -114,7 +146,7 @@ for dep in expo-cli turtle-cli; do
     else
         echo_pass ${package_installed[1]} ${package_installed[2]}
     fi
-done
+done < "$file2"
 
 if [[ $DEPS_INSTALLED = "false" ]]; then
     echo
